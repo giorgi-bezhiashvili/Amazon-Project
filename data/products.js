05131bf31a983601e9ebe864b92412557a9e18b4
Giorgi bezhiashvili <giorgibezhiashvili5@gmail.com>
2025-02-24 11:56:16 +0400

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,14 +1,53 @@
-export function getProduct(productId){
-  let matchingProduct
+import { formatCurrency } from "../Js/utils/money.js";
+
+export function getProduct(productId) {
+  let matchingProduct;
 
   products.forEach((product) => {
     if (product.id === productId) {
-      matchingProduct = product
+      matchingProduct = product;
     }
-  })
+  });
 
-  return matchingProduct
+  return matchingProduct;
 }
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+}
+
+const Product1 = new Product({
+  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+  rating: {
+    stars: 4.5,
+    count: 87,
+  },
+  priceCents: 1090,
+  keywords: ["socks", "sports", "apparel"],
+});
+console.log(Product1);
 
 
 export const products = [
@@ -697,4 +736,8 @@ export const products = [
       "school"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
+
+console.log(products);
