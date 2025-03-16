5f4148d0f7d2cec0ca3c05fdf002d52166daf19c
Giorgi bezhiashvili <giorgibezhiashvili5@gmail.com>
2025-03-16 10:55:38 +0400

    Update checkout.js

--- Js/checkout.js
+++ Js/checkout.js
@@ -1,6 +1,20 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSumaary.js";
-//import "../data/cart-class.js"
-
-renderOrderSummary()
-renderPaymentSummary()
+import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        
+        resolve()
+    })
+}).then(()=>{
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve()
+        })
+    })
+    
+}).then(()=>{
+    renderOrderSummary()
+    renderPaymentSummary()
+})
